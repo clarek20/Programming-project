e151d28e6744302f082af5590ef9e5d55f6c40d3
Sam Nalty <sam@snalty.xyz>
2019-11-21 11:19:53 +0000

    Added functions for plotting individual ccg data

--- api.py
+++ api.py
@@ -49,6 +49,41 @@ def get_merged_dataframe(org, code):
     result = pd.merge(n_items_df, list_size_df)
     return result
 
+def get_practice_info(query):
+    request_url = "https://openprescribing.net/api/1.0/org_code/"
+    # GET /api/1.0/org_details/?org_type=practice&org=99H&keys=total_list_size
+    params = {
+        "org_type": "practice",
+        "q": query,
+        "format": "json"
+    }
+    op_request = requests.get(request_url, params=params)
+    op_request_dict = json.loads(op_request.text)
+    if len(op_request_dict) > 1:
+        print(f"The query returned more than 1 practice, automatically choosing the first practice which is {op_request_dict[0]['name']}")
+        op_request_dict = op_request_dict[0]
+    elif len(op_request_dict) == 0:
+        raise Exception(f"Query {query} returned no results, perhaps the query was missspelled.")
+    else:
+        # if query only returns one result, return that result
+        op_request_dict = op_request_dict[0]
+
+    return op_request_dict
+
+
+def get_practice_prescriptions_by_time(practice, ccg='14L', bnf='5.1'):
+    df = get_merged_dataframe(ccg, bnf)
+    practice = get_practice_info(practice)['code']
+    filtered_df = df[df.row_id == practice]
+    return filtered_df
+
+def plot_practice_prescriptions_by_time(practice, ccg='14L', bnf='5.1'):
+    df = get_practice_prescriptions_by_time(practice, ccg, bnf)
+    # Add function later to get name of bnf paragraph or section
+    title = f"Number of prescriptions by {practice} over time"
+    plot = df.plot(x = 'date', y = 'items', title=title)
+    return plot 
+    
 
 def op_request_to_dataframe(dictionary):
     df = pd.DataFrame.from_dict(dictionary)
